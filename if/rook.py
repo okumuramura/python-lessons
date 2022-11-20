7cbd00254f6c3bbe1918af16c35245f676bd66e9
okumuramura <bugagaga88@gmail.com>
2022-11-20 13:12:51 +0400

    solved rook

--- if/rook.py
+++ if/rook.py
@@ -7,3 +7,11 @@
 Выходные данные
 Требуется вывести слово YES, если ладья сможет побить фигуру за 1 ход и NO - в противном случае.
 '''
+
+x1, y1 = map(int, input().split())
+x2, y2 = map(int, input().split())
+
+if x1 == x2 or y1 == y2:
+    print('YES')
+else:
+    print('NO')
