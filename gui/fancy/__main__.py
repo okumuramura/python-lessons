daa28ef40d73a14fdb4666302cfefb5fbf7bdea9
okumuramura <bugagaga88@gmail.com>
2023-04-23 13:02:04 +0400

    colorpick

--- gui/fancy/__main__.py
+++ gui/fancy/__main__.py
@@ -1,9 +1,20 @@
+import random
+
 from PySide6 import QtWidgets, QtGui, QtCore
 
 class FancyButton(QtWidgets.QPushButton):
-    def __init__(self, parent, text: str):
+    def __init__(self, parent: QtWidgets.QWidget, text: str):
         QtWidgets.QPushButton.__init__(self, parent)
         self.setText(text)
+    
+    def enterEvent(self, event: QtGui.QEnterEvent) -> None:
+        p_box = self.parentWidget().rect()
+
+        new_x = random.randint(0, p_box.width() - self.rect().width())
+        new_y = random.randint(0, p_box.height() - self.rect().height())
+
+        self.move(new_x, new_y)
+        return super().enterEvent(event)
 
 class MainWindow(QtWidgets.QWidget):
     def __init__(self):
@@ -12,7 +23,13 @@ class MainWindow(QtWidgets.QWidget):
         self.resize(800, 600)
 
         self.btn = FancyButton(self, "Нажми меня :)")
+        self.btn.resize(200, 50)
+        bbox = self.btn.rect()
+        self.btn.move(400 - bbox.width() // 2, 300 - bbox.height() // 2)
+        self.btn.clicked.connect(self.win)
 
+    def win(self):
+        self.setWindowTitle("You won!")
 
 app = QtWidgets.QApplication()
 
---
+++ gui/widgets/__main__.py
@@ -0,0 +1,24 @@
+from PySide6 import QtWidgets, QtGui, QtCore
+
+class MainWindow(QtWidgets.QWidget):
+    def __init__(self):
+        QtWidgets.QWidget.__init__(self)
+
+        self.resize(800, 600)
+        
+        self.select_color_btn = QtWidgets.QPushButton(self)
+        bbox = self.select_color_btn.rect()
+        self.select_color_btn.move(400 - bbox.width() // 2, 300 - bbox.height())
+        self.select_color_btn.clicked.connect(self.color_pick)
+
+    def color_pick(self):
+        color_picker = QtWidgets.QColorDialog(self)
+        color = color_picker.getColor()
+        print(color.name())
+
+app = QtWidgets.QApplication()
+
+window = MainWindow()
+window.show()
+
+app.exec()
